e1b2dc7a951b14b0ad67f41ca23c35ddfb4452d1
rafeeth <rafeeth03@gmail.com>
2024-10-25 17:04:29 +0530

    first commit

---
+++ try3YT/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.IO.Compression;
+using System.Diagnostics;
+//using UpdateFunction;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+using System.Deployment.Application;
+using System.Net.NetworkInformation;
+
+namespace UpdateService
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // Start the update check
+
+        }
+    }
+}
