cb049d0ad69bda6751d0202cdd5a346405d1d28e
rafeeth <rafeeth03@gmail.com>
2024-10-28 17:38:29 +0530

    check for update button added

--- try3YT/Form1.Designer.cs
+++ try3YT/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -39,15 +40,26 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Hi :)";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(669, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "check for updates";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnCheckForUpdates);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Main";
-            this.Text = "Tool v1.0";
+            this.Text = "Tool v1.1";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
 
@@ -56,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- try3YT/Form1.cs
+++ try3YT/Form1.cs
@@ -19,6 +19,7 @@ using System.Net;
 using System.Runtime.ConstrainedExecution;
 using System.Deployment.Application;
 using System.Net.NetworkInformation;
+using System.Security.Policy;
 
 namespace UpdateService
 {
@@ -32,6 +33,47 @@ namespace UpdateService
         {
             // Start the update check
 
+        }
+        private void btnCheckForUpdates(object sender, EventArgs e)
+        {
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                try
+                {
+                    ApplicationDeployment deployment = ApplicationDeployment.CurrentDeployment;
+                    UpdateCheckInfo updateInfo = deployment.CheckForDetailedUpdate();
+                    if (updateInfo.UpdateAvailable)
+                    {
+                        DialogResult result = MessageBox.Show("An update is available. Would you like to install it?", "Update Available", MessageBoxButtons.YesNo);
+                        if (result == DialogResult.Yes)
+                        {
+                            deployment.Update();
+                            MessageBox.Show("The application has been updated and will now restart.", "Update Complete");
+                            Application.Restart();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No updates are available.", "Check for Updates");
+                    }
+                }
+                catch (DeploymentDownloadException ex)
+                {
+                    MessageBox.Show("The update could not be downloaded. Please check your network connection and try again.", "Update Error");
+                }
+                catch (InvalidDeploymentException ex)
+                {
+                    MessageBox.Show("The ClickOnce deployment is not valid. Please contact the application vendor.", "Deployment Error");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show("This application cannot check for updates. It may not be a ClickOnce application.", "Update Check Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("This application is not deployed using ClickOnce.", "Update Check Error");
+            }
         }
     }
 }
